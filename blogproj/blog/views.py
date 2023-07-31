3daee5eada8f68572cc7e85ff24c0d94210dc48c
Parikshit Hiwase <parikshithiwase@gmail.com>
2023-08-01 01:44:55 +0530

    Upto database

---
+++ blogproj/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+posts=[
+    {
+    'author':'Parikshit Hiwase',
+    'title':'Blog Post 1',
+    'content':'Test blog post',
+    'date_posted':'July 31 2023',
+    },
+    {
+    'author':'Test User',
+    'title':'Test Blog Post',
+    'content':'Test blog post 2',
+    'date_posted':'July 31 2023',
+    }
+
+]
+
+def home(request):
+    # return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts' : posts 
+    }
+    return render(request,'blog/home.html',context)
+
+
+def about(request):
+    # return HttpResponse("<h1>Blog About</h1>")
+    return render(request,'blog/about.html', {'title': "About Page"})
