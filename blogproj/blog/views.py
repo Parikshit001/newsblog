41570ffcf67de2568ba13d6b2f2d7ce46f0425ec
Parikshit Hiwase <parikshithiwase@gmail.com>
2023-08-01 04:12:29 +0530

    Donw with login and register

--- blogproj/blog/views.py
+++ blogproj/blog/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-posts=[
-    {
-    'author':'Parikshit Hiwase',
-    'title':'Blog Post 1',
-    'content':'Test blog post',
-    'date_posted':'July 31 2023',
-    },
-    {
-    'author':'Test User',
-    'title':'Test Blog Post',
-    'content':'Test blog post 2',
-    'date_posted':'July 31 2023',
-    }
+# posts=[
+#     {
+#     'author':'Parikshit Hiwase',
+#     'title':'Blog Post 1',
+#     'content':'Test blog post',
+#     'date_posted':'July 31 2023',
+#     },
+#     {
+#     'author':'Test User',
+#     'title':'Test Blog Post',
+#     'content':'Test blog post 2',
+#     'date_posted':'July 31 2023',
+#     }
 
-]
+# ]
+
 
 def home(request):
     # return HttpResponse("<h1>Blog Home</h1>")
     context = {
-        'posts' : posts 
+        'posts': Post.objects.all()
     }
-    return render(request,'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
     # return HttpResponse("<h1>Blog About</h1>")
-    return render(request,'blog/about.html', {'title': "About Page"})
+    return render(request, 'blog/about.html', {'title': "About Page"})
--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 
     #own
     "blog.apps.BlogConfig",
+    'user.apps.UserConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_TEMPLATE_PACK = "bootstrap6"
+MEDIA_ROOT = ''
+MEDIA_URL = ''
